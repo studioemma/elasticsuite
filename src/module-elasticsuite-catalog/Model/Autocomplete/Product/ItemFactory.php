54fda012dd6cdd70c0c1aabca69ef95847a9943c
Quinten Clause <quinten.clause@studioemma.eu>
2018-03-30 11:21:41 +0200

    Show sku in search results

--- src/module-elasticsuite-catalog/Model/Autocomplete/Product/ItemFactory.php
+++ src/module-elasticsuite-catalog/Model/Autocomplete/Product/ItemFactory.php
@@ -99,6 +99,7 @@ class ItemFactory extends \Magento\Search\Model\Autocomplete\ItemFactory
         $productData = [
             'title' => $product->getName(),
             'image' => $this->getImageUrl($product),
+            'sku'   => $product->getSku(),
             'url'   => $product->getProductUrl(),
             'price' => $this->renderProductPrice($product, \Magento\Catalog\Pricing\Price\FinalPrice::PRICE_CODE),
         ];
